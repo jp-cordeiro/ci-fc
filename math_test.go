bd62652a4cddcc6b937793649ddfac5ee21e318c
jp-cordeiro <jpc.araujo@hotmail.com>
2024-07-28 22:49:16 -0300

    test: fail test

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	total := Sum(10, 10)
+	total := Sum(10, 20)
 
 	if total != 20 {
 		t.Errorf("Resultado inválido: Resulta %d. Esperado %d", total, 20)
