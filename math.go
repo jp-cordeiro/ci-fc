7d83a796c164db78de7783bd43853bf4fc38869b
jp-cordeiro <jpc.araujo@hotmail.com>
2024-10-06 16:17:20 -0300

    ci: add sonarcloud

--- math.go
+++ math.go
@@ -9,3 +9,22 @@ func main() {
 func Sum(a int, b int) int {
 	return a + b
 }
+func Sum0(a int, b int) int {
+	return a + b
+}
+
+func Sum1(a int, b int) int {
+	return a + b
+}
+func Sum2(a int, b int) int {
+	return a + b
+}
+func Sum3(a int, b int) int {
+	return a + b
+}
+func Sum4(a int, b int) int {
+	return a + b
+}
+func Sum5(a int, b int) int {
+	return a + b
+}
